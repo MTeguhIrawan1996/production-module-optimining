9a88bea5dfe07b6218b93a47ff1d45e0d361da29
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2023-12-21 19:04:20 +0700

    :lipstick: style(app): create components global form steper at stockpile monitoring create

---
+++ src/utils/form-validation/stockpile-monitoring/stockpile-monitoring-validation.ts
@@ -0,0 +1,55 @@
+import { z } from 'zod';
+
+import {
+  zDateOptionalValidation,
+  zDateValidation,
+  zImageArrayOptional,
+  zOptionalString,
+  zRequiredSelectInput,
+} from '@/utils/form-validation/global';
+
+export const stockpileMonitoringMutationValidation: z.ZodType<any> = z.object({
+  stockpileId: zRequiredSelectInput,
+  handbookId: zOptionalString,
+  domeId: zRequiredSelectInput,
+  oreSubMaterialId: zRequiredSelectInput,
+  openDate: zDateValidation,
+  openTime: zOptionalString,
+  closeDate: zDateValidation,
+  closeTime: zOptionalString,
+  desc: zOptionalString,
+  photo: zImageArrayOptional,
+  // tonSurveys: z.object({
+  //   ton: zOptionalString,
+  //   date: zDateValidation,
+  // }).array()
+  // bargingStartDate?: Date | null;
+  // bargingStartTime: string;
+  // bargingFinishDate?: Date | null;
+  // bargingFinishTime: string;
+  // movings: {
+  //   startDate?: Date | null;
+  //   startTime: string;
+  //   finishDate?: Date | null;
+  //   finishTime: string;
+  // }[];
+  // reopens: {
+  //   openDate?: Date | null;
+  //   openTime: string;
+  //   closeDate?: Date | null;
+  //   closeTime: string;
+  // }[];
+  samples: z
+    .object({
+      date: zDateOptionalValidation,
+      sampleTypeId: zOptionalString.nullable(),
+      sampleNumber: zOptionalString,
+      elements: z
+        .object({
+          elementId: zOptionalString.nullable(),
+          value: zOptionalString,
+        })
+        .array(),
+    })
+    .array(),
+});
