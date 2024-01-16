6ef462af048670473cb06204618db4172db86f73
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2024-01-16 12:49:28 +0700

    :construction: wip(app): adjust handle submit at update front and weather production

--- src/components/features/InputData/Productions/data-front/update/common/sections/UpdateFrontProductionBook.tsx
+++ src/components/features/InputData/Productions/data-front/update/common/sections/UpdateFrontProductionBook.tsx
@@ -337,7 +337,12 @@ const UpdateFrontProductionBook = () => {
         submitButton={{
           label: t('commonTypography.save'),
           type: 'button',
-          onClick: () => setIsOpenConfirmation((prev) => !prev),
+          onClick: async () => {
+            const output = await methods.trigger(undefined, {
+              shouldFocus: true,
+            });
+            if (output) setIsOpenConfirmation((prev) => !prev);
+          },
         }}
         backButton={{
           onClick: () =>
