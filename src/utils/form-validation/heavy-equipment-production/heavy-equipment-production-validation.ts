26d601f31a4555e05618f44fe47da5444d046662
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2024-01-17 13:48:09 +0700

    :sparkles: feat(app): feature Create Hour Meter at heavy equipment production

--- src/utils/form-validation/heavy-equipment-production/heavy-equipment-production-validation.ts
+++ src/utils/form-validation/heavy-equipment-production/heavy-equipment-production-validation.ts
@@ -3,34 +3,44 @@ import { z } from 'zod';
 import { IMutationHeavyEquipmentDataProdValues } from '@/services/graphql/mutation/heavy-equipment-production/useCreateHeavyEquipmentProduction';
 import {
   zDateValidation,
+  zOptionalNumber,
   zOptionalString,
+  zRequiredNumber,
   zRequiredSelectInput,
   zRequiredString,
 } from '@/utils/form-validation/global';
 
 export const heavyEquipmentProductionMutationValidation: z.ZodType<IMutationHeavyEquipmentDataProdValues> =
-  z.object({
-    date: zDateValidation,
-    foremanId: zRequiredSelectInput,
-    operatorId: zRequiredSelectInput,
-    companyHeavyEquipmentId: zRequiredSelectInput,
-    shiftId: zRequiredSelectInput,
-    workStartTime: zRequiredString,
-    workFinishTime: zRequiredString,
-    amountWorkTime: zOptionalString,
-    desc: zOptionalString,
-    heavyEquipmentType: zOptionalString,
-    loseTimes: z
-      .object({
-        workingHourPlanId: zOptionalString,
-        name: zOptionalString,
-        amountHour: zOptionalString,
-        details: z
-          .object({
-            startTime: zRequiredString,
-            finishTime: zRequiredString,
-          })
-          .array(),
-      })
-      .array(),
-  });
+  z
+    .object({
+      date: zDateValidation,
+      foremanId: zRequiredSelectInput,
+      operatorId: zRequiredSelectInput,
+      companyHeavyEquipmentId: zRequiredSelectInput,
+      shiftId: zRequiredSelectInput,
+      workStartTime: zRequiredString,
+      workFinishTime: zRequiredString,
+      amountWorkTime: zOptionalString,
+      desc: zOptionalString,
+      heavyEquipmentType: zOptionalString,
+      hourMeterBefore: zRequiredNumber,
+      hourMeterAfter: zRequiredNumber,
+      amountHourMeter: zOptionalNumber,
+      loseTimes: z
+        .object({
+          workingHourPlanId: zOptionalString,
+          name: zOptionalString,
+          amountHour: zOptionalString,
+          details: z
+            .object({
+              startTime: zRequiredString,
+              finishTime: zRequiredString,
+            })
+            .array(),
+        })
+        .array(),
+    })
+    .refine((data) => data.hourMeterBefore < data.hourMeterAfter, {
+      message: 'HM awal harus lebih kecil dari HM akhir',
+      path: ['hourMeterBefore'], // path of error
+    });
