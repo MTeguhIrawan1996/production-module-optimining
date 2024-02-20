30191973d681ac9997c3daf54940bd9e5a6ecc3f
rizal ghod <rijalghodi10@gmail.com>
2024-02-20 20:12:24 +0700

    :lipstick: style(component): color brand last breadcrumb

--- src/components/elements/global/Breadcrumb.tsx
+++ src/components/elements/global/Breadcrumb.tsx
@@ -1,38 +1,41 @@
-import {
-  Breadcrumbs as MantineBreadcrumbs,
-  createStyles,
-  Text,
-} from '@mantine/core';
+import { Breadcrumbs as MantineBreadcrumbs } from '@mantine/core';
+import { IconChevronRight } from '@tabler/icons-react';
 import Link from 'next/link';
 import * as React from 'react';
+import { shallow } from 'zustand/shallow';
 
 import { useBreadcrumbs } from '@/utils/store/useBreadcrumbs';
 
-const useStyles = createStyles((theme) => ({
-  breadcrumbStyle: {
-    cursor: 'pointer',
-    textDecoration: 'none',
-    color: theme.colors.dark[6],
-    fontSize: 14,
-    '&:hover': {
-      color: theme.colors.brand[6],
-    },
-  },
-}));
-
 const Breadcrumb: React.FC = () => {
-  const { classes } = useStyles();
-
-  // const { breadcrumbs } = useBreadcrumbs((state) => state);
-
-  const { breadcrumbs } = useBreadcrumbs((state) => state);
+  const { breadcrumbs } = useBreadcrumbs((state) => state, shallow);
 
   return (
-    <MantineBreadcrumbs separator={<span> / </span>}>
+    <MantineBreadcrumbs
+      separator={<IconChevronRight size={16} />}
+      styles={(theme) => ({
+        separator: {
+          color: theme.colors.dark[6],
+          marginLeft: 8,
+          marginRight: 8,
+        },
+        breadcrumb: {
+          cursor: 'pointer',
+          textDecoration: 'none',
+          color: theme.colors.dark[6],
+          fontSize: 14,
+          '&:hover': {
+            color: theme.colors.brand[6],
+          },
+          '&:last-of-type': {
+            color: theme.colors.brand[6],
+          },
+        },
+      })}
+    >
       {breadcrumbs?.map(({ label, path }) => {
         return (
           <Link href={path} key={label} prefetch={false}>
-            <Text className={classes.breadcrumbStyle}>{label}</Text>
+            {label}
           </Link>
         );
       })}
@@ -41,7 +44,3 @@ const Breadcrumb: React.FC = () => {
 };
 
 export default Breadcrumb;
-
-/**
- *
- */
