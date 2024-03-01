c372603720a5efc184a7320e42d5c822b50e5cb0
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2024-03-01 16:01:42 +0700

    :recycle: refactor(lib): improve function getWeekInMonth handle lastWeekYear

--- src/utils/helper/getWeeksInMonth.ts
+++ src/utils/helper/getWeeksInMonth.ts
@@ -1,32 +1,42 @@
 import dayjs from './dayjs.config';
 
 export const getWeeksInMonth = (year: string, month: string) => {
-  // Menghitung jumlah hari dalam bulan tersebut
-  const daysInMonth = dayjs(`${year}-${month}`).daysInMonth();
-
   // Mendapatkan tanggal pertama dari bulan tersebut
   const firstDayOfMonth = dayjs()
     .year(Number(year))
     .month(Number(month) - 1)
     .startOf('month');
 
+  const lastDateOfMonth = dayjs()
+    .year(Number(year))
+    .month(Number(month) - 1)
+    .endOf('month');
+
   // Mendapatkan hari keberapa tanggal pertama tersebut dalam seminggu (0: Minggu, 1: Senin, dst.)
   const firstDayOfWeek = firstDayOfMonth.day();
-
-  // const weekOfMonth = firstDayOfMonth.isoWeek();
   // mendapatkan hari pertama tersebut berada pada minggu ke berapa dalam setahun
-  const weekOfMonth = firstDayOfMonth.week();
+  const firstWeekOfMonth = firstDayOfMonth.week();
+  const lastWeekOfMonth = lastDateOfMonth.week();
 
-  // const adjustWeekOfMonth =
-  //   firstDayOfWeek === 0 ? weekOfMonth + 1 : weekOfMonth;
+  // Menghitung jumlah hari dalam bulan tersebut
+  const daysInMonth = dayjs(`${year}-${month}`).daysInMonth();
+
+  // console.log(firstWeekOfMonth);
+  // console.log(lastWeekOfMonth);
+  // console.log((daysInMonth + firstDayOfWeek) / 7);
 
   // Menghitung jumlah minggu berdasarkan jumlah hari dalam bulan dan hari pertama dalam seminggu
-  const weeks = Math.ceil((daysInMonth + firstDayOfWeek) / 7);
+  const weeks =
+    firstWeekOfMonth >= lastWeekOfMonth
+      ? Math.ceil((daysInMonth + firstDayOfWeek) / 7) - 1
+      : Math.ceil((daysInMonth + firstDayOfWeek) / 7);
+
+  // console.log(weeks);
 
   // Membuat array yang berisi nomor-nomor minggu dalam bulan tersebut
   const weeksArray = Array.from(
     { length: weeks },
-    (_, index) => index + weekOfMonth
+    (_, index) => index + firstWeekOfMonth
   );
 
   return weeksArray;
