ae2cc079fdb582b1d233f6eaa93c34d25bf9a364
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2024-03-25 10:28:34 +0700

    :bug: fix(pages): handle nullable in update weather production issue (256)

--- src/services/graphql/mutation/weather-production/useCreateWeatherProduction.ts
+++ src/services/graphql/mutation/weather-production/useCreateWeatherProduction.ts
@@ -34,7 +34,7 @@ export interface IMutationWeatherProductionValues {
     conditionId: string | null;
     startTime: string;
     finishTime: string;
-    rainfall: number | null | '';
+    rainfall: number | string | null;
   }[];
 }
 
