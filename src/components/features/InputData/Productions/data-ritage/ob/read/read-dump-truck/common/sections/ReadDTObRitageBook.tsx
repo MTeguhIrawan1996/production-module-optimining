ffca303c1e7594257b336738c1ce799eba41b840
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2024-05-17 17:26:02 +0700

    :recycle: refactor(app): takeout action in list material (Pra Plan)

--- src/components/features/InputData/Productions/data-ritage/ob/read/read-dump-truck/common/sections/ReadDTObRitageBook.tsx
+++ src/components/features/InputData/Productions/data-ritage/ob/read/read-dump-truck/common/sections/ReadDTObRitageBook.tsx
@@ -97,6 +97,7 @@ const ReadDTObRitageBook = () => {
             render: ({ tonByRitage }) => tonByRitage ?? '-',
           },
         ]}
+        subMaterialHidden
         onOpenModal={onOpenModal}
         // tabs="ob"
         fetching={detailsObRitageDTDataLoading}
