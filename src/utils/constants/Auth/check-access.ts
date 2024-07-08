09a59ae65f788187e51bff3b3444425f1896b021
MTeguhIrawan1996 <teguhirawan1996@gmail.com>
2024-07-08 13:42:19 +0700

    :recycle: refactor(app): ajust middleware check access permission

---
+++ src/utils/constants/Auth/check-access.ts
@@ -0,0 +1,124 @@
+export const checkAccess = [
+  {
+    path: '/dashboard',
+    moduleSlug: [{ slug: 'dashboard' }],
+  },
+  {
+    path: '/master-data/company',
+    moduleSlug: [{ slug: 'company' }],
+  },
+  {
+    path: '/master-data/human-resources',
+    moduleSlug: [{ slug: 'human-resource' }],
+  },
+  {
+    path: '/master-data/heavy-equipment',
+    moduleSlug: [{ slug: 'heavy-equipment' }],
+  },
+  {
+    path: '/master-data/location',
+    moduleSlug: [{ slug: 'location' }],
+  },
+  {
+    path: '/master-data/block',
+    moduleSlug: [{ slug: 'block' }],
+  },
+  {
+    path: '/master-data/stockpile',
+    moduleSlug: [{ slug: 'stockpile' }],
+  },
+  {
+    path: '/master-data/material',
+    moduleSlug: [{ slug: 'material' }],
+  },
+  {
+    path: '/master-data/working-hours-plan',
+    moduleSlug: [{ slug: 'working-hour-plan' }],
+  },
+  {
+    path: '/master-data/activity-plan',
+    moduleSlug: [{ slug: 'activity-plan' }],
+  },
+  {
+    path: '/master-data/element',
+    moduleSlug: [{ slug: 'elemnt' }],
+  },
+  {
+    path: '/master-data/shift',
+    moduleSlug: [{ slug: 'shift' }],
+  },
+  {
+    path: '/master-data/factory',
+    moduleSlug: [{ slug: 'factory' }],
+  },
+  {
+    path: '/master-data/activity-category',
+    moduleSlug: [
+      {
+        slug: 'working-hour-plan-category',
+      },
+      {
+        slug: 'heavy-equipment-data-formula',
+      },
+    ],
+  },
+  {
+    path: '/reference/company-type',
+    moduleSlug: [{ slug: 'company-type' }],
+  },
+  {
+    path: '/reference/heavy-equipment-class',
+    moduleSlug: [{ slug: 'heavy-equipment-class' }],
+  },
+  {
+    path: '/reference/heavy-equipment',
+    moduleSlug: [{ slug: 'heavy-equipment-reference' }],
+  },
+  {
+    path: '/input-data/quality-control-management/stockpile-monitoring',
+    moduleSlug: [{ slug: 'monitoring-stockpile' }],
+  },
+  {
+    path: '/input-data/quality-control-management/sample-house-lab',
+    moduleSlug: [{ slug: 'house-sample-and-lab' }],
+  },
+  {
+    path: '/input-data/quality-control-management/shipping-monitoring',
+    moduleSlug: [{ slug: 'monitoring-barging' }],
+  },
+  {
+    path: '/input-data/production/data-ritage',
+    moduleSlug: [
+      { slug: 'ore-ritage' },
+      { slug: 'overburden-ritage' },
+      { slug: 'quarry-ritage' },
+      { slug: 'barging-ritage' },
+      { slug: 'moving-ritage' },
+      { slug: 'topsoil-ritage' },
+    ],
+  },
+  {
+    path: '/input-data/production/data-heavy-equipment',
+    moduleSlug: [{ slug: 'heavy-equipment-data' }],
+  },
+  {
+    path: '/input-data/production/data-weather',
+    moduleSlug: [{ slug: 'weather-data' }],
+  },
+  {
+    path: '/input-data/production/data-front',
+    moduleSlug: [{ slug: 'front-data' }],
+  },
+  {
+    path: '/input-data/production/map',
+    moduleSlug: [{ slug: 'map-data' }],
+  },
+  {
+    path: '/plan/weekly',
+    moduleSlug: [{ slug: 'weekly-plan' }],
+  },
+  {
+    path: '/plan/monthly',
+    moduleSlug: [{ slug: 'monthly-plan' }],
+  },
+];
